45ceb454dc4074fe29fe4465b693a73cfab388d4
liu <602694077@qq.com>
2018-04-11 17:29:08 +0800

    2018年4月11日 17:28:47  创建splashActivity界面

--- app/src/main/java/example/ldgd/com/mymobileplayer2/SplashActivity.java
+++ app/src/main/java/example/ldgd/com/mymobileplayer2/SplashActivity.java
@@ -3,11 +3,13 @@ package example.ldgd.com.mymobileplayer2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+
     }
 }
