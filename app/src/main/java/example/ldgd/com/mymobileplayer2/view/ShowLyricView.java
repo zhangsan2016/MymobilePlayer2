adaf2b2c7525cf63e21b2134c175a81e55b2f5e1
liu <602694077@qq.com>
2018-07-23 18:18:06 +0800

    2018年7月23日 17:50:31 设置歌词测试3

--- app/src/main/java/example/ldgd/com/mymobileplayer2/view/ShowLyricView.java
+++ app/src/main/java/example/ldgd/com/mymobileplayer2/view/ShowLyricView.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
